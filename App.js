f7db74ffed31996e65686f92c83ab29cfc22a284
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-03-25 11:38:47 -0300

    .

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import * as imagePicker from 'expo-image-picker'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <></>
+      </View>
+      <View>
+        <></>
+      </View>
     </View>
   );
 }
