f40fbe8f7646abed1cac1ac1fc163db2b4f612d3
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-03-25 11:58:03 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-import * as imagePicker from 'expo-image-picker'
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
 
 export default function App() {
+  const [foto, setFoto] = useState("");
+
+  const [status, requestPermission] = ImagePicker.useCameraPermissions(); 
   return (
+    <>
+      <StatusBar />
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View>
-        <></>
+      <View style={styles.campoFoto}>
+          <View style={styles.viewFoto}>
+
+          </View>
+          <View>
+            <Button title='Tirar Foto'/>
+          </View>
       </View>
-      <View>
-        <></>
+      <View style={styles.campoMapa}>
+          <View style={styles.viewMapa}>
+            <MapView style={styles.mapa}/>
+          </View>
+          <View>
+            <Button title='Pegar localização'/>
+          </View>
       </View>
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  viewFoto: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  viewMapa: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  mapa: {
+    width: "70%",
+    height: "70%"
+  },
+  campoFoto: {
+    flex: 1,
+    backgroundColor: "green"
+  },
+  campoMapa: {
+    backgroundColor: "yellow",
+    flex: 1
+  }
 });
