58404e2336074ab0b8b7cc77215677a6a477a57d
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-02 09:11:50 -0300

    Update Home.js

--- screen/Home.js
+++ screen/Home.js
@@ -1,34 +1,41 @@
-import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-
-
+import { Pressable, StyleSheet, Text, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function Home({ navigation }) {
   return (
-    <SafeAreaView style={styles.subcontainer}>
-      <StatusBar/>
-      <ScrollView >
-        <View style={styles.viewHome}>
-          <View>
-          <Text style={styles.titulo}>Momentos Bons</Text>
-          </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.subcontainer}>
+        <View style={styles.viewGeral}>
+          
+            <View style={styles.viewHome}>
+              <View>
+                <Text style={styles.titulo}>Momentos Bons</Text>
+              </View>
 
-          <View style={styles.campoBotao}>
-            <Pressable style={styles.botao} onPress={() => navigation.navigate("RegistrarLugar")}>
-              <Text style={styles.textoBotao}>Criar um momento</Text>
-            </Pressable>
+              <View style={styles.campoBotao}>
+                <Pressable
+                  style={styles.botao}
+                  onPress={() => navigation.navigate("RegistrarLugar")}
+                >
+                  <Text style={styles.textoBotao}>Criar um momento</Text>
+                </Pressable>
 
-            <Pressable style={styles.botao} onPress={() => navigation.navigate("MomentosSalvos")}>
-              <Text style={styles.textoBotao}>Momentos Salvos</Text>
-            </Pressable>
-          </View>
-
+                <Pressable
+                  style={styles.botao}
+                  onPress={() => navigation.navigate("MomentosSalvos")}
+                >
+                  <Text style={styles.textoBotao}>Momentos Salvos</Text>
+                </Pressable>
+              </View>
+            </View>
+          
         </View>
-    </ScrollView>
-    </SafeAreaView>
-  )
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -36,14 +43,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "yellow",
   },
+  viewGeral: {
+    flex: 1,
+    backgroundColor: "red",
+    justifyContent: "center"
+  },
   viewHome: {
     alignItems: "center",
     backgroundColor: "#EEEEEE",
+    
+    justifyContent: "center"
   },
   titulo: {
     fontSize: 30,
     marginBottom: 50,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   campoBotao: {
     flexDirection: "row",
@@ -53,10 +67,9 @@ const styles = StyleSheet.create({
   botao: {
     backgroundColor: "#2b2b2b",
     padding: 10,
-
   },
   textoBotao: {
     color: "#EEEEEE",
-    fontSize: 15
-  }
-})
+    fontSize: 15,
+  },
+});
